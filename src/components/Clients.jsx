eff691a6d98c3a8ffb06916d5a00bf24d7e79c17
unknown <oluwatoyosiolaniyan@gmail.com>
2024-01-18 03:30:41 -0800

    18th 2nd

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { clients } from "../constants/constants";
+import styles from "../style";
 
-const Clients = () => {
-  return <div>Clients</div>;
-};
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4 `}>
+    <div className={`${styles.flexCenter} flex-wrap w-full `}>
+      {clients.map((client) => (
+        <div
+          key={client.id}
+          className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5 hover:bg-sky-200 bg-primary-100 cursor-pointer rounded-full`}
+        >
+          <img
+            src={client.logo}
+            alt="client_logo"
+            className="sm:w-[192px] w-[100px] object-contain py-3 hover:text-secondary"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
 export default Clients;
